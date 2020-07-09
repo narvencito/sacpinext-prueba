ecfeaf17180195836ea52ba7f28a18d7fe4b406e
narven <narvencito@gmail.com>
2020-07-09 16:26:04 -0500

    first commit

--- src/config/sacpinext-ui.env.js
+++ src/config/sacpinext-ui.env.js
@@ -1,5 +1,5 @@
 window.SacpiNextUIEnv = {
-    "apiUrl": "http://localhost:62381/api"//"{{ .Env.SACPI_API_URL }}"
-    //"apiUrl": "http://apinext.couchpotatofries.org:8788/api"
+    //"apiUrl": "http://localhost:62381/api"//"{{ .Env.SACPI_API_URL }}"
+    "apiUrl": "http://apinext.couchpotatofries.org:8788/api"
 };
   
